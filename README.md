3970ab4f7ce5416562584d841bc2e556c56986bd
Zhen Zhang <izgzhen@gmail.com>
2020-01-26 13:36:41 -0800

    Update graph import analysis

--- README.md
+++ README.md
@@ -6,15 +6,21 @@ Android/Java toolkit with Python API based on https://github.com/izgzhen/liquid.
 ## Dependencies
 
 - Python 3
+  + `poetry installl; poetry shell`
 - Java > 8
+- `git clone https://github.com/semantic-graph/semantic-graph.py semantic`
 - `git clone https://github.com/izgzhen/android-platforms`
 
 ## Features
 
-Import graph visualization (`<depth>` is the maximum package namespace depth):
+Import graph visualization (`<depth>` is the maximum package namespace depth, `<op>` can be `-` or `=`):
 
 ```
-python3 import-graph.py path/to/some.apk path/to/output.dot <depth>
+python3 import-graph.py path/to/some.apk path/to/output.dot <depth> <op><filter-list>
+
+# e.g.
+python3 import-graph.py path/to/some.apk path/to/output.dot 2 -com.google,com.facebook
+python3 import-graph.py ~/tmp/pinball.apk ~/tmp/pinball.dot 2 '=com.bingo,com.unity,com.chartboost'
 ```
 
 ## About pre-compiled jar
--- import-graph.py
+++ import-graph.py
@@ -8,34 +8,67 @@ import json
 import tempfile
 import os
 import subprocess
+import yaml
+
+from semantic.graph import Graph
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 apk = os.path.realpath(sys.argv[1])
 out = os.path.realpath(sys.argv[2])
 depth = int(sys.argv[3])
+filter_pkgs = []
+preserve_pkgs = None
+if len(sys.argv) > 4:
+    arg = sys.argv[4]
+    if arg.startswith("-"):
+        filter_pkgs = arg[1:].split(",")
+    elif arg.startswith("="):
+        preserve_pkgs = arg[1:].split(",")
 
 os.chdir(SCRIPT_DIR)
 
-tmp = tempfile.NamedTemporaryFile(delete=False).name
-subprocess.check_output(["./liquid", apk, "callgraph.txt", tmp])
+output_file = "tmp/" + os.path.basename(apk).replace(".apk", "") + "-callgraph.json"
+if not os.path.exists(output_file):
+    subprocess.check_output(["./liquid", apk, "callgraph.txt", output_file])
 
-bindings = json.loads(open(tmp, "r").read())
+cfg = yaml.safe_load(open("config.yaml", "r").read())
+pkg_prefixes = cfg["whitelistPackagePrefixes"]
 
-added_links = set()
+bindings = json.loads(open(output_file, "r").read())
 
 def class_pkg(clazz):
-    return ".".join(clazz.split(".")[:depth])
+    for pkg in pkg_prefixes:
+        if clazz.startswith(pkg):
+            return pkg
+    return ".".join(clazz.split(".")[:depth]).split("$")[0]
+
+edges = []
+for b in bindings:
+    callee = class_pkg(b["gclass"])
+    caller = class_pkg(b["C"])
+    if callee is None or caller is None or callee == caller:
+        continue
+    edges.append((caller, callee))
+
+g = Graph()
+g.add_edges(edges)
+
+to_kill = []
+for node in g.nodes:
+    if preserve_pkgs is not None:
+        if all(not node.startswith(pkg) for pkg in preserve_pkgs):
+            to_kill.append(node)
+    else:
+        for pkg in filter_pkgs:
+            if node.startswith(pkg):
+                to_kill.append(node)
+
+print("Remove: %s" % to_kill)
+g.remove_nodes_soft(to_kill)
 
 with open(out, "w") as f:
     f.write("digraph {\n")
-    for b in bindings:
-        callee = '"' + class_pkg(b["gclass"]) + '"'
-        caller = '"' + class_pkg(b["C"]) + '"'
-        if callee == caller:
-            continue
-        link = callee + " -> " + caller
-        if link not in added_links:
-            added_links.add(link)
-            f.write(link + "\n")
+    for u, v in g.edges:
+        f.write('"' + u + '" -> "' + v + '"\n')
     f.write("}\n")
